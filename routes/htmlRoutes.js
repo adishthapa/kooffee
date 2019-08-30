c8cd710c1efef2d114e32e83d0ce427ab6c737b1
adishthapa <adishthapa@outlook.com>
2019-08-29 20:11:04 -0400

    Fixed travis-ci issue

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,5 @@
-var db = require("../models");
-
 module.exports = function(app) {
-  // Load index page
   app.get("/", function(req, res) {
-    // db.Example.findAll({}).then(function(dbExamples) {
-    //   res.render("index", {
-    //     msg: "Welcome!",
-    //     examples: dbExamples
-    //   });
-    // });
     res.render("index");
   });
 
